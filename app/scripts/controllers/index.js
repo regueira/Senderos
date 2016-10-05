2a04885911430e6228edac1eb85ccdd29c568ad6
Sebastian Regueira <sebastian.regueira@globallogic.com>
2016-10-05 10:55:58 -0300

    se elimina el navigate para que no haya confusión

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -1,13 +1,7 @@
 import { router } from '../modules/Router';
 
 export default function( req ) {
-    var profile;
 
     console.log( 'controller / asdasdasd' );
 
-    profile = document.getElementById( 'profile' );
-
-    profile.onclick = function() {
-        router.navigate( '/profile/123/hola/123' );
-    };
 }
